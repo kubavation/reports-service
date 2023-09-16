2648b54d39bb85e7cdbec88c839a2d9b178f5488
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-16 18:37:24 +0200

    fix

--- src/main/java/com/durys/jakub/reportsservice/pattern/domain/ReportPatternInfo.java
+++ src/main/java/com/durys/jakub/reportsservice/pattern/domain/ReportPatternInfo.java
@@ -1,16 +1,20 @@
 package com.durys.jakub.reportsservice.pattern.domain;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class ReportPatternInfo {
     private String name;
     private String description;
     private String subsystem;
+
+    public ReportPatternInfo(String name, String description, String subsystem) {
+        this.name = name;
+        this.description = description;
+        this.subsystem = subsystem;
+    }
 }
--- src/main/java/com/durys/jakub/reportsservice/pattern/infrastructure/ReportPatternRepository.java
+++ src/main/java/com/durys/jakub/reportsservice/pattern/infrastructure/ReportPatternRepository.java
@@ -11,7 +11,9 @@ public interface ReportPatternRepository extends CrudRepository<ReportPattern, L
     @Query("select p.file from ReportPattern p where p.informations.name = :reportName and p.informations.subsystem = :subsystem")
     Optional<byte[]> filePatternOf(String reportName, String subsystem);
 
-    @Query("select new com.durys.jakub.reportsservice.pattern.domain.ReportPatternInfo(p.informations.name, p.informations.description, p.informations.subsystem) " +
-           " from ReportPattern p where p.informations.name = :reportName and p.informations.subsystem = :subsystem")
+    @Query(""" 
+           select new com.durys.jakub.reportsservice.pattern.domain.ReportPatternInfo(p.informations.name, p.informations.description, p.informations.subsystem)
+           from ReportPattern p where p.informations.name = :reportName and p.informations.subsystem = :subsystem
+           """)
     Optional<byte[]> patternInformations(String reportName, String subsystem);
 }
