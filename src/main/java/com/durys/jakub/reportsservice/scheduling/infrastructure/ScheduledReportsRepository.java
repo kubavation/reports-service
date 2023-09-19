8cf3ed89e3fbe7b4dc2179355fc3cf2ca7c8027d
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-19 20:52:39 +0200

    fix in query

--- src/main/java/com/durys/jakub/reportsservice/scheduling/infrastructure/ScheduledReportsRepository.java
+++ src/main/java/com/durys/jakub/reportsservice/scheduling/infrastructure/ScheduledReportsRepository.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 public interface ScheduledReportsRepository extends JpaRepository<ScheduledReport, Long> {
 
-    @Query(value = "SELECT sr.* FROM REP_SCHEDULED_REPORT sr where TO_CHAR(sr.at, 'yyyy-mm-dd HH24:MI') = TO_CHAR(:at, 'yyyy-mm-dd HH24:MI') ",
+    @Query(value = "SELECT sr.* FROM REP_SCHEDULED_REPORT sr where TO_CHAR(sr.at, 'yyyy-mm-dd HH24:MI') = :at ",
             nativeQuery = true)
     Set<ScheduledReport> findScheduled(LocalDateTime at);
 }
