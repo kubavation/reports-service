bd36b262d0c75f9a1d4d2699eef8c7057a63411c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-21 18:27:37 +0200

    ReportPatternController documentation

--- src/main/java/com/durys/jakub/reportsservice/pattern/infrastructure/in/ReportPatternController.java
+++ src/main/java/com/durys/jakub/reportsservice/pattern/infrastructure/in/ReportPatternController.java
@@ -6,6 +6,7 @@ import com.durys.jakub.reportsservice.pattern.infrastructure.in.model.PatternPar
 import com.durys.jakub.reportsservice.pattern.infrastructure.in.model.ReportPatternDTO;
 import com.durys.jakub.reportsservice.sharedkernel.model.ReportPatternInfo;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,25 +29,29 @@ public class ReportPatternController {
     @Operation(description = "List of patterns based on subsystem")
     @ApiResponse(responseCode = "200", description = "List of patterns")
     @GetMapping("/subsystem/{subsystem}")
-    public Set<ReportPatternInfo> patterns(@Param("Subsystem") @PathVariable String subsystem) {
+    public Set<ReportPatternInfo> patterns(@Parameter(description ="Subsystem") @PathVariable String subsystem) {
         return reportPatternRepository.subsystemPatterns(subsystem);
     }
 
     @Operation(description = "List of pattern parameters")
     @ApiResponse(responseCode = "200", description = "List of parameters")
     @GetMapping("/{patternId}/parameters")
-    public Set<PatternParameterDTO> patternParameters(@Param("Pattern ID") @PathVariable Long patternId) {
+    public Set<PatternParameterDTO> patternParameters(@Parameter(description ="Pattern ID") @PathVariable Long patternId) {
         return reportPatternRepository.patternParams(patternId);
     }
 
+    @Operation(description = "Create report pattern")
+    @ApiResponse(responseCode = "200", description = "Report pattern successfully created")
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public void createPattern(@RequestPart ReportPatternDTO pattern, @RequestPart MultipartFile file) throws Exception {
+    public void createPattern(@Parameter(description = "Pattern definition") @RequestPart ReportPatternDTO pattern,
+                              @Parameter(description = "Pattern file") @RequestPart MultipartFile file) throws Exception {
         reportPatternApplicationService.create(pattern, file);
     }
 
     @PutMapping(path = "/{patternId}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public void editPattern(@PathVariable Long patternId,
-            @RequestPart ReportPatternDTO pattern, @RequestPart MultipartFile file) throws Exception {
+    public void editPattern(@Parameter(description = "Pattern ID")  @PathVariable Long patternId,
+                            @Parameter(description = "Pattern definition") @RequestPart ReportPatternDTO pattern,
+                            @Parameter(description = "Pattern file") @RequestPart MultipartFile file) throws Exception {
         reportPatternApplicationService.edit(patternId, pattern, file);
     }
 }
