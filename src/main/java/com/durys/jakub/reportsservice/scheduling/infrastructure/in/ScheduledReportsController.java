07061b18ea6ef3b04d6ef990fe6c7f1668ea6d8c
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-26 20:54:47 +0200

    endpoint for retrieving scheduled reports

---
+++ src/main/java/com/durys/jakub/reportsservice/scheduling/infrastructure/in/ScheduledReportsController.java
@@ -0,0 +1,25 @@
+package com.durys.jakub.reportsservice.scheduling.infrastructure.in;
+
+import com.durys.jakub.reportsservice.scheduling.infrastructure.ScheduledReportDTO;
+import com.durys.jakub.reportsservice.scheduling.infrastructure.ScheduledReportsRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/scheduled-reports")
+@RequiredArgsConstructor
+public class ScheduledReportsController {
+
+    private final ScheduledReportsRepository scheduledReportsRepository;
+
+    @GetMapping
+    public List<ScheduledReportDTO> findAllScheduled() {
+        return scheduledReportsRepository.findAllScheduled();
+    }
+}
