1ab943835ecf4b9b194dce8b61f163bde0a2af0b
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-28 19:33:20 +0200

    generationType field in pattern dto

--- src/main/java/com/durys/jakub/reportsservice/generator/ReportGenerator.java
+++ src/main/java/com/durys/jakub/reportsservice/generator/ReportGenerator.java
@@ -1,39 +1,45 @@
 package com.durys.jakub.reportsservice.generator;
 
 import com.durys.jakub.reportsservice.api.model.ReportCreationParam;
-import com.durys.jakub.reportsservice.pattern.application.ReportPatternApplicationService;
 import com.durys.jakub.reportsservice.api.model.ReportFormat;
+import com.durys.jakub.reportsservice.pattern.application.ReportPatternApplicationService;
 import com.durys.jakub.reportsservice.sharedkernel.model.GeneratedReport;
+import com.durys.jakub.reportsservice.sharedkernel.model.ReportPatternInfo;
 import lombok.RequiredArgsConstructor;
-import net.sf.jasperreports.engine.*;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
 import org.springframework.stereotype.Component;
 
-import javax.sql.DataSource;
-import java.io.*;
 import java.nio.file.Path;
 import java.util.Set;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class ReportGenerator {
 
     private final ReportPatternApplicationService reportPatternService;
-    private final DataSource dataSource;
+    private final ReportParametersService reportParametersService;
 
     public GeneratedReport generate(String reportName, String subsystem,
                                     Set<ReportCreationParam> reportParams, ReportFormat format) throws Exception {
 
+        log.info("generating report {} for subsystem {}", reportName, subsystem);
+
+        ReportPatternInfo patternInfo = reportPatternService.reportPatternInfo(reportName, subsystem);
         Path filePath = reportPatternService.patternFilePath(reportName, subsystem);
 
         JasperReport report = ReportCache.compiledReport(filePath)
                 .getOrElseGet(r -> compile(filePath));
 
-        JasperPrint generated = ReportParametersService.fill(report, reportParams, dataSource);
+        JasperPrint generated = reportParametersService.fill(report, reportParams, patternInfo);
 
         return new GeneratedReport(
                 ReportPrintService.print(generated, format),
-                reportName,
-                format.format());
+                reportName, format.format());
     }
 
 
