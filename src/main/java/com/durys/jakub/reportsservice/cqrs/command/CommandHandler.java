8fc307303a8dfd9e97a52d8e9a46bfb49cd6a5be
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-01 21:02:24 +0200

    ScheduleReportGenerationCommand + handler

--- src/main/java/com/durys/jakub/reportsservice/cqrs/command/CommandHandler.java
+++ src/main/java/com/durys/jakub/reportsservice/cqrs/command/CommandHandler.java
@@ -1,5 +1,5 @@
 package com.durys.jakub.reportsservice.cqrs.command;
 
-public interface CommandHandler<T extends Command, R> {
+public interface CommandHandler<T extends Command<R>, R> {
     R handle(T command);
 }
