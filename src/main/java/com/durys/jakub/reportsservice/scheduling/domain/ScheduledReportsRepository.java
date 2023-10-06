b930f47c29403d6cea50a0921bf34f3814105564
kubavation <37585627+kubavation@users.noreply.github.com>
2023-10-06 19:15:57 +0200

    build fix

--- src/main/java/com/durys/jakub/reportsservice/scheduling/domain/ScheduledReportsRepository.java
+++ src/main/java/com/durys/jakub/reportsservice/scheduling/domain/ScheduledReportsRepository.java
@@ -12,13 +12,13 @@ import java.util.Set;
 public interface ScheduledReportsRepository extends JpaRepository<ScheduledReport, Long> {
 
     @Query(value = "SELECT sr.* FROM REP_SCHEDULED_REPORT sr " +
-            "INNER JOIN REP_REPORT r on sr.report_id = r.id where TO_CHAR(r.at, 'yyyy-mm-dd') = :at ", nativeQuery = true)
+            "INNER JOIN REP_REPORT r on sr.report_id = r.id where TO_CHAR(r.scheduled_at, 'yyyy-mm-dd') = :at ", nativeQuery = true)
     Set<ScheduledReport> findScheduled(Date at);
 
     @Query("""
             select new com.durys.jakub.reportsservice.scheduling.infrastructure.ScheduledReportDTO(
             r.id, r.patternInformations.name, r.patternInformations.description, r.patternInformations.subsystem,
-            r.fileName, sr.status, sr.at)
+            r.fileName, sr.status, sr.scheduleAt)
            from Report r
            join ScheduledReport sr on sr.reportId = r.id
            """)
